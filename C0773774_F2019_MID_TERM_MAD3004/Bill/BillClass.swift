7be60eef709e6eaa8af5e3196455715b9f0acb92
nathanjaswal <nathanjaswal@gmail.com>
2019-10-19 17:54:07 -0400

    [Nitin] Bill Class Created

---
+++ C0773774_F2019_MID_TERM_MAD3004/Bill/BillClass.swift
@@ -0,0 +1,15 @@
+//
+//  BillClass.swift
+//  C0773774_F2019_MID_TERM_MAD3004
+//
+//  Created by Apple on 19/10/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import Foundation
+
+/// This is a bill main class
+class NJBillClass {
+
+
+}
