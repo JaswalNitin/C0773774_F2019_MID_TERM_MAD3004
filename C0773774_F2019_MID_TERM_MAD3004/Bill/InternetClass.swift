bc5320bd86b4be8769e46f500dc06664f3f03a94
nathanjaswal <nathanjaswal@gmail.com>
2019-10-19 17:58:25 -0400

    [Nitin] Internet Class created

---
+++ C0773774_F2019_MID_TERM_MAD3004/Bill/InternetClass.swift
@@ -0,0 +1,15 @@
+//
+//  InternetClass.swift
+//  C0773774_F2019_MID_TERM_MAD3004
+//
+//  Created by Apple on 19/10/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import Foundation
+
+/// this is the class to define the operation and values of Internet Bill
+class NJInternetClass: NJBillClass {
+
+
+}
