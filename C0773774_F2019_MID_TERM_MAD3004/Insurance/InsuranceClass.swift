6b017ae557ccc04207da719639f0144993bff89d
nathanjaswal <nathanjaswal@gmail.com>
2019-10-19 20:18:40 -0400

    [nitin] insurance class created

---
+++ C0773774_F2019_MID_TERM_MAD3004/Insurance/InsuranceClass.swift
@@ -0,0 +1,37 @@
+//
+//  InsuranceClass.swift
+//  C0773774_F2019_MID_TERM_MAD3004
+//
+//  Created by Apple on 19/10/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import Foundation
+
+
+class IsuranceClass { //:  IPrintP
+    
+//    // MARK: - Properties
+//    var name = String()
+//
+//    var data = Float()
+    
+    
+    
+    // MARK: - Life Cycle
+    /// initializing the Internet Class
+    
+    
+    // MARK: - Functions
+//    /// this is a protocol method to print data
+//    func displayData() {
+//        // print data of parent class(Bill Class)
+//        super.printData()
+//
+//        // print local variables
+//        print("   Provider Name: ", self.name)
+//        let data = self.final_data
+//        print("   Internet Usage: ", data)
+//    }
+    
+}
